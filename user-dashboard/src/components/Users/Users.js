dbd97a4ce5267d7233c35ee7579d32cd4b7566da
Judy <judy.zhu@bindo.com>
2018-04-20 00:45:20 +0800

    finish step7

---
+++ user-dashboard/src/components/Users/Users.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styles from './Users.css';
+import { connect } from 'dva';
+import { Table, Pagination, Popconfirm } from 'antd';
+
+function Users({ list: dataSource, total, page: current }) {
+
+  function deleteHandler(id) {
+    console.warn(`TODO: ${id}`);
+  }
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a href="">{text}</a>,
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Website',
+      dataIndex: 'website',
+      key: 'website',
+    },
+    {
+      title: 'Operation',
+      key: 'operation',
+      render: (test, { id }) => (
+        <span className={styles.operation}>
+          <a href="">Edit</a>
+          <Popconfirm
+            title="Confirm to delete?"
+            onConfirm={deleteHandler.bind(null, id)}>
+            <a href="">Delete</a>
+          </Popconfirm>
+        </span>
+      ),
+    },
+  ]
+  return (
+    <div className={styles.normal}>
+      <div>
+        <Table
+          columns={columns}
+          dataSource={dataSource}
+          rowKey={record => record.id}
+          pagination={false}/>
+        <Pagination
+          className="ant-table-pagination"
+          total={total}
+          current={current}
+          pageSize={3}/>
+      </div>
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  const { list, total, page } = state.users;
+  return { list, total, page }
+}
+
+export default connect(mapStateToProps)(Users);
